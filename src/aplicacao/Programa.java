daa9b493890ad49f29f0e4aa606c88ca2fc8036d
MuriloWisch <158103499+MuriloWisch@users.noreply.github.com>
2025-08-04 10:38:13 -0300

    position class

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,5 +1,8 @@
+import boardgame.Position;
+
 public class Programa {
     public static void main(String[] args) {
-
+        Position position = new Position(3,5);
+        System.out.println(position);
     }
 }
