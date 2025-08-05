bdbc232faf538c92d4015e926185af23bcc48f1f
MuriloWisch <158103499+MuriloWisch@users.noreply.github.com>
2025-08-04 21:15:37 -0300

    Pieces add

---
+++ src/aplicacao/xadrez/pieces/King.java
@@ -0,0 +1,15 @@
+package xadrez.pieces;
+
+import boardgame.Board;
+import xadrez.ChessPiece;
+import xadrez.Color;
+
+public class King extends ChessPiece {
+    public King(Board board, Color color) {
+        super(board, color);
+    }
+    @Override
+    public String toString(){
+        return "K";
+    }
+}
