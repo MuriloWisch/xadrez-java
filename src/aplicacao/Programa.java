857c55cfc2766e2fac9a18d8dfbc4683046bc0ca
MuriloWisch <158103499+MuriloWisch@users.noreply.github.com>
2025-08-06 12:16:30 -0300

    movimento de peças

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,9 +1,27 @@
 
 import xadrez.ChessMatch;
+import xadrez.ChessPiece;
+import xadrez.ChessPosition;
+
+import java.util.Scanner;
 
 public class Programa {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         ChessMatch chessMatch = new ChessMatch();
-        UI.printBoard(chessMatch.getPieces());
+
+        while (true){
+            UI.printBoard(chessMatch.getPieces());
+            System.out.println();
+            System.out.print("Source: ");
+            ChessPosition source = UI.readChessPosition(scanner);
+
+            System.out.println();
+            System.out.println("Target: ");
+            ChessPosition target = UI.readChessPosition(scanner);
+
+            ChessPiece capturedPiece = chessMatch.performChessMove(source,target);
+        }
+
     }
 }
